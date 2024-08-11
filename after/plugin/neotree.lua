4dc2ddc152c64e89ff712b32eaf3368c6eb676bd
austinburnett <aburn1011@gmail.com>
2024-08-11 19:45:05 -0400

    Disable neo-tree netrw overriding

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -4,6 +4,8 @@
                 visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
                 hide_dotfiles = false,
                 hide_gitignored = true,
-            }
+            },
+            -- I use netrw so disable neo-tree
+            hijack_netrw_behavior =  "disabled"
         }
     })
