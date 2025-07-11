62982bf62cfc7b6a22d6674811bf5c01d6a59aa7
austinburnett <aburn1011@gmail.com>
2025-07-10 21:17:42 -0400

    Small changes to configs

--- lua/init.lua
+++ lua/init.lua
@@ -2,5 +2,5 @@ require("set")
 require("remap")
 require("config.lazy")
 
-vim.diagnostic.config({ virtual_lines = true})
+--vim.diagnostic.config({ virtual_lines = true})
 vim.diagnostic.config({ virtual_text = true})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,8 +11,9 @@ return {
             },
         },
         config = function()
-            require("lspconfig").lua_ls.setup{}
-            require("lspconfig").clangd.setup{}
+            local capabilities = require("blink.cmp").get_lsp_capabilities()
+            require("lspconfig").lua_ls.setup{capabilities = capabilities}
+            require("lspconfig").clangd.setup{capabilities = capabilities}
         end,
     }
 }
